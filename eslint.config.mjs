6ddd7b24a7e3560e41e6d9652d6e1ab9fa1b8314
ComputerGenie <tim@yourcomputergenie.com>
2025-11-01 07:11:49 -0500

    chore(lint): add root eslint.config.mjs to enforce modern style rules

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+export default [
+  {
+    files: ['**/*.js'],
+    ignores: ['website/public/lib/*.min.js'],
+    languageOptions: {
+      'ecmaVersion': 2022,
+      'sourceType': 'commonjs',
+      'globals': {
+        'console': 'readonly',
+        'process': 'readonly',
+        'Buffer': 'readonly',
+        '__dirname': 'readonly',
+        '__filename': 'readonly',
+        'require': 'readonly',
+        'module': 'readonly',
+        'exports': 'readonly',
+        'BigInt': 'readonly',
+        'algos': 'readonly'
+      },
+    },
+    'rules': {
+      'brace-style': ['error', '1tbs'],
+      'curly': ['error', 'all'],
+      'indent': ['error', 4, { 'SwitchCase': 1 }],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+      'prefer-template': 'error',
+      'prefer-arrow-callback': 'error',
+      'semi': ['error', 'always'],
+      'no-throw-literal': 'off',
+      'no-prototype-builtins': 'off'
+    },
+  },
+];
